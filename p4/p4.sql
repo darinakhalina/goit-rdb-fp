8882cfefd28e9d2baccd868be2af4ebdb801200e
Darina Khalina <darina.khalina@namecheap.com>
2024-12-01 21:23:25 +0200

    clean up

--- p4/p4.sql
+++ p4/p4.sql
@@ -2,7 +2,7 @@ USE pandemic;
 
 SELECT 
     year,
-	MAKEDATE(year, 1) AS first_day_date,
+    MAKEDATE(year, 1) AS first_day_date,
     CURDATE() AS `current_date`,
     TIMESTAMPDIFF(YEAR, MAKEDATE(year, 1), CURDATE()) AS difference
 FROM infectious_cases_updated;
